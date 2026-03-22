1c1e35d35d4d47f1a730b2ac4d7e84c9971ed182
sahilsinghchouhan <161635928+sahilsinghchouhan@users.noreply.github.com>
2026-03-23 00:31:38 +0530

    datatypes

---
+++ 01_basics/basics_01.js
@@ -0,0 +1,20 @@
+// const accountID = '22246'
+
+// let name= 'sahil'
+// var email = '@gmail.com'
+
+// password= '12345'
+
+
+
+// const define kene k badd apn change nhi kr skte h
+// let not block scope problem so we use let instead of var 
+// without use let var const u cant define anything ,this is very bad thing in javascript
+
+
+// console.table([accountID,name,email,password]);
+
+
+
+console.log('sahil');
+
