1d46a721453fc6cf40ea173aab26f74370b46090
sahilsinghchouhan <161635928+sahilsinghchouhan@users.noreply.github.com>
2026-03-23 15:38:44 +0530

    conversions

---
+++ 01_basics/conversions.js
@@ -0,0 +1,25 @@
+let score ="33" ;
+
+
+//console.log(typeof score);
+let toStrings = Number(score);
+
+// console.log(typeof toStrings);
+// console.log(toStrings);
+
+
+let isLogged = 0;
+
+console.log(typeof isLogged);
+
+let isboolean = Boolean(isLogged);
+
+console.log(typeof isLogged);
+console.log( isLogged);
+
+console.log(typeof isboolean);
+console.log( isboolean);
+
+
+
+
